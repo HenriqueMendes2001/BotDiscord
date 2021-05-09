7656c60c57c0c2a73f4fccf5e1123b2337ea8917
Henrique Silva Mender <iquinhosilva@hotmail.com>
2021-05-09 11:57:37 -0300

    Criando funções do prefixo

--- bot.py
+++ bot.py
@@ -1,16 +1,57 @@
 # bot.py
 import os
-
+import json
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+#Determina quais prefixos iremos utilizar
+def get_prefix(client, message):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    return prefixes [str(message.guild.id)]
+
+client = commands.Bot(command_prefix = get_prefix)
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+#Manipula os dados do prefix.json
+@client.event
+async def on_guild_join(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes [str(guild.id)] = 'o!'
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+@client.event
+async def on_guild_remove(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+@client.command()
+async def changeprefix(ctx, prefix):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes [str(ctx.guild.id)] = prefix
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
 
 client.run(TOKEN)
